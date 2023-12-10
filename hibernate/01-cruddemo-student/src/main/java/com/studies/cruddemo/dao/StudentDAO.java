a0474a9fc7683fdd487676d267c5a46ce1a1fb6c
David Rabko <david.rabko@icloud.com>
2023-12-10 23:40:16 +0100

    Finish jpa crud

--- hibernate/01-cruddemo-student/src/main/java/com/studies/cruddemo/dao/StudentDAO.java
+++
@@ -1,9 +0,0 @@
-package com.studies.cruddemo.dao;
-
-import com.studies.cruddemo.entity.Student;
-
-public interface StudentDAO {
-    void save(Student student);
-
-    Student findById(Integer id);
-}
